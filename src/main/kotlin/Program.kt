2ce28bfb95b0234b4670c1801b0ee0a15be4b279
jonathana <jaap5227@gmail.com>
2018-06-24 20:25:08 -0500

    Using functional approach

--- src/main/kotlin/Program.kt
+++ src/main/kotlin/Program.kt
@@ -1,17 +1,31 @@
+import com.jaap.kotlin.ProviderDetails
 import com.jaap.kotlin.Providers
 
 fun main(args: Array<String>) {
 
+    val providers = Providers()
+
     // With instance
-    Providers().apply { p -> println("\t${p.name} has ${p.size} items") }
+    providers.apply { p -> println("\t${p.name} has ${p.size} items") }
 
 
     // With companion
-    val providers = Providers.getProviders()
-
-    providers.forEach {
-        provider -> println("Name ${provider.name}")
+    Providers.getProviders().forEach { provider ->
+        println("Name ${provider.name}")
         provider.forEach { key, value -> println("\t$key : $value") }
     }
+
+
+    // Using Functions
+    val providerFormatter = { detail: ProviderDetails -> "${detail.providerName} -> ${detail.name}" }
+    val providerFormatter2 : (ProviderDetails) -> String = { "${it.providerName} -> ${it.name}"}
+
+    println("\nPrinting all =================")
+    providers.getProviderDetails()
+            .forEach { println(providerFormatter(it)) } // Printing
+    println("\nPrinting filtered =================")
+    providers.getFilteredProviderDetails("random")
+            .map(providerFormatter2)// Transforming to printable string
+            .forEach(::println) // Printing
 
 }
--- src/main/kotlin/com/jaap/kotlin/Providers.kt
+++ src/main/kotlin/com/jaap/kotlin/Providers.kt
@@ -4,10 +4,40 @@ import java.security.Provider
 import java.security.Security
 
 
+data class ProviderDetails (val providerName : String, val name : String)
+
 class Providers {
 
     fun apply(fn : (Provider) -> Unit) {
         getProviders().forEach { p -> fn(p) }
+    }
+
+    fun getProviderDetails() : List<ProviderDetails> {
+
+        // Create a mutable list to add mapped values
+        val details = mutableListOf<ProviderDetails>()
+
+        getProviders().forEach {
+
+            // Mapping all entries of every provider
+            p -> val providerDetails = p.entries.map { entry -> ProviderDetails(p.name, entry.key.toString()) }
+            // Adding every provider detail list into global detail list
+            details.addAll(providerDetails)
+            // can be done as:
+            // details += providerDetails
+        }
+
+        return details
+    }
+
+    fun getFilteredProviderDetails(filter : String) : List<ProviderDetails> {
+
+        // Using flat map
+        return getProviders().flatMap {
+            // There's no need of using item name, you can simply use "it"
+            p -> p.entries.filter { it.key.toString().contains(filter, true) }
+                .map { ProviderDetails(p.name, it.key.toString()) }
+        }
     }
 
     companion object {
